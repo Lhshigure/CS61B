320062d0060c2f0206cb7cc75a2e1feebf93d24c
Lhshigre <luhang696997@gmail.com>
2023-06-28 20:51:59 +1000

    second time

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -25,8 +25,8 @@ public class NBody{
 	}
 
 	public static void main(String[] args) {
-		double T = Double.parseDouble(args[0]);
-		double dt = Double.parseDouble(args[1]);
+		double T = new Double(args[0]);
+		double dt = new Double(args[1]);
 		String filename = args[2];
 
 		double r = readRadius(filename);
@@ -59,6 +59,14 @@ public class NBody{
 			}
 			StdDraw.show();
 			StdDraw.pause(10);
+			t += dt;
+		}
+		StdOut.printf("%d\n", ps.length);
+		StdOut.printf("%.2e\n", r);
+		for (int i = 0; i < ps.length; i++) {
+			StdOut.printf("%11.4e %11.4e %11.4e %11.4e %11.4e %12s\n",
+					ps[i].xxPos, ps[i].yyPos, ps[i].xxVel,
+					ps[i].yyVel, ps[i].mass, ps[i].imgFileName);
 		}
 	}
 }
